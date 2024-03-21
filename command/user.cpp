e50010ec31d3d83787879a10c42b66ec47533911
Burak Karlıdağ <burakkarlidag23@gmail.com>
2024-03-22 02:03:18 +0300

    Makefile

--- command/user.cpp
+++ command/user.cpp
@@ -1,9 +1,52 @@
 
 #include "command.hpp"
 
-void    USER(Client &client, Server &server)
-{
-    
+void user(std::vector<Client>& clients, Client& ite, Server& server) {
+    std::string message;
+
+    if (ite.isRegistered == true) {
+        message = ERR_ALREADYREGISTRED();
+        send(ite.socket_fd, message.c_str(), message.size(), 0);
+        return;
+    }
+
+    if (ite.commands.size() < 5) {
+        message = ERR_NEEDMOREPARAMS();
+        send(ite.socket_fd, message.c_str(), message.size(), 0);
+        return;
+    }
+
+    std::string buffer;
+    for (int i = 0; i < ite.commands.size(); i++) {
+        if (ite.commands[i][0] == ':' && i == 4) {
+            buffer += (&ite.commands[i][1]);
+            while (++i < ite.commands.size()) {
+                buffer += " ";
+                buffer += ite.commands[i];
+            }
+            break;
+        } else {
+            buffer += ite.commands[i];
+            buffer += " ";
+        }
+    }
+
+    std::vector<std::string> userService;
+    size_t pos = 0;
+    while ((pos = buffer.find(' ')) != std::string::npos) {
+        userService.push_back(buffer.substr(0, pos));
+        buffer.erase(0, pos + 1);
+    }
+
+    if (userService.size() != 5 && userService[2][0] != '0' && userService[3][0] != '*') {
+        message = ite.getPrefix() + ": Error: Wrong userparameters\r\n";
+        send(ite.socket_fd, message.c_str(), message.size(), 0);
+        return;
+    }
+
+    ite.set_username(userService[1]);
+    ite.set_realname(userService[4]);
+
+	std::cout << "userName: " + ite.get_username() << "\nrealName" + ite.get_realname()<< std::endl;
+
 }
-
-
---
+++ sil.cpp
@@ -0,0 +1,94 @@
+#include "client.hpp"
+#include "server.hpp"
+int commandInterface(std::string buffer, Client *client, std::vector <Client> clients, Server &server)
+{
+    std::string pass;
+    std::string nick;
+    std::string user;
+    std::string real;
+    std::stringstream stringStream(buffer);
+    std::vector <std::string> splitStrings;
+    for (std::string line; std::getline(stringStream, line, '\n'); )
+        splitStrings.push_back(line);
+    std::vector <std::string>::iterator iter = splitStrings.begin();
+    if (iter->size() > 4 && iter->substr(0, 4) == "PASS")
+        pass = iter->substr(6, iter->find(13, 6) - 6), iter++;
+    if (iter->size() > 4 && iter->substr(0, 4) == "NICK")
+        nick = iter->substr(5, iter->find(13, 5) - 5), iter++;
+    else
+        return 0;
+    if (iter->size() > 4 && iter->substr(0, 4) == "USER")
+    {
+        user = iter->substr(5, iter->find(' ', 5) - 5);
+        real = iter->substr(iter->find(':') + 5);
+    }
+    else
+        return 0;
+    if (pass == server.getPassword())
+        client->isRegistered = true, client->isPassed = true;
+    client->set_username(user);
+    client->set_nickname(nick);
+    client->set_realname(real.erase(real.size() - 1));
+    return 1;
+}
+
+void    commandParser(std::string buffer,std::vector<Client> clients,Client &ite)
+{
+    int a;
+    if (buffer[buffer.length() - 2] == '\r')
+        a = buffer.length() - 2;
+    else
+        a = buffer.length() - 1;
+    std::string temp = buffer.substr(0,a);
+    int index;
+    int flag;
+    for(int i = 0; i < a; i++)
+    {
+        flag = 0;
+        while (temp[i] == ' ' && i < a)
+            i++;
+        if (i < a)
+            index = i;
+        while (temp[i] != ' ' && i < a)
+        {
+            flag = 1;
+            i++;
+        }
+        if (flag == 1)
+            ite.commands.push_back(temp.substr(index, i - index));
+    }
+    for (const std::string& command : ite.commands) {
+        std::cout << "$" << command << std::endl;
+    }
+ 
+ }
+
+void commandSearch(std::vector<Client> clients, Client *ite, Server *server, std::vector <Channel> *channels)
+{
+    std::cout << "commandSearch"<<"userName: " + ite->get_username() << "\nrealName" + ite->get_realname()<< std::endl;
+    std::vector<std::string>::iterator k = ite->commands.begin();
+    if ("PASS" == *k)
+    {
+       std::cout << "in pass" << std::endl; 
+       pass(*ite, *server);
+    }
+    else if ("USER" == *k)
+    {
+        user(clients, *ite, *server);
+    }
+    
+}
+
+
+void handleBuffer(std::string buffer, Client *client, std::vector <Client> clients, Server * server, std::vector <Channel> *channels)
+{
+    if (commandInterface(buffer, client, clients, *server))
+    {
+         std::cout << "command interface return" << std::endl;
+         return ;
+    }
+    commandParser(buffer, clients, *client);
+    commandSearch(clients, client, server, channels);
+}
+
+ 
