be0b5a42d8bcfec918082d7ae01f77a740d5c567
Burak Karlıdağ <burakkarlidag23@gmail.com>
2024-03-28 03:35:25 +0300

    join command created

---
+++ command/join.cpp
@@ -0,0 +1,65 @@
+
+#include "command.hpp"
+
+void	join_info(std::vector<Client> clients, Client &ite, Channel &channels)
+{
+	std::string names;
+	std::vector <Client *> a= channels.users;
+	for(std::vector <Client *>::iterator itera = a.begin() ; itera != a.end(); itera++)
+	{
+		if(itera == a.begin())
+		{
+			names = "@";
+			names += (*itera)->get_nickname();
+		}
+		else
+		{
+			names += " ";
+			names += (*itera)->get_nickname();
+		}
+	}
+	for(std::vector <Client *>::iterator itera = a.begin() ; itera != a.end(); itera++)
+	{
+		message_join((*itera), RPL_NAMREPLY((*itera)->get_nickname(), channels.ChannelName, names));
+		message_join((*itera), RPL_ENDOFNAMES((*itera)->get_nickname(), channels.ChannelName));
+	}
+}
+int	join_check(std::vector <Channel>::iterator itChannels, Client &ite)
+{
+	for(std::vector <Client *>::iterator itera = itChannels->users.begin() ; itera != itChannels->users.end(); itera++)
+	{
+		if((*itera)->get_nickname() == ite.get_nickname())
+			return (1);
+	}
+	return (0);
+}
+void	join(std::vector<Client> clients, Client &ite, Server &server, std::vector <Channel> &channels)
+{
+	std::vector<std::string>::iterator k = ite.commands.begin();
+	*k++;
+	if(k[0][0] == '#')
+	{
+		for (std::vector<Channel>::iterator itChannels = channels.begin(); itChannels != channels.end(); itChannels++)
+		{
+			if(itChannels->ChannelName == *k)
+			{
+				if(join_check(itChannels, ite) == 1)
+					return ;
+				Client *userPtr1 = &ite;
+				itChannels->createUser(userPtr1);
+				ite.isjoined.push_back(*itChannels);
+				sendmessage(ite, "JOIN You are now in channel " + itChannels->ChannelName);
+				join_info(clients, ite, *itChannels);
+				return ;
+			}
+		}
+		Client *userPtr = &ite;
+		Channel newChannel = Channel((*k), userPtr, userPtr);
+		ite.isjoined.push_back(newChannel);
+		channels.push_back(newChannel);
+		sendmessage(ite, " JOIN You are now in channel " + newChannel.ChannelName);
+		std::string buffer = RPL_NOTOPIC(ite.get_nickname(), newChannel.ChannelName);
+		message_for_topic(ite, RPL_NOTOPIC(ite.get_nickname(), newChannel.ChannelName));
+		join_info(clients, ite, newChannel);
+	}
+}
---
+++ command/mode.cpp
@@ -0,0 +1,40 @@
+
+#include "command.hpp"
+
+void MODE(std::vector<Client> clients, Client ite, Server &server, std::vector <Channel> channels)
+{
+	std::vector<std::string>::iterator k = ite.commands.begin();
+	std::string names;
+
+	if(k[2] == "+o")
+	{
+		for (std::vector<Channel>::iterator itChannels = channels.begin(); itChannels != channels.end(); itChannels++)
+		{
+
+			if(itChannels->ChannelName == k[1])
+			{
+				names = '@' + k[3];
+				sendmessage(ite, "Mode " + itChannels->ChannelName + " +o " +k[3]);
+				for(std::vector <Client *>::iterator itera = itChannels->users.begin() ; itera != itChannels->users.end(); itera++)
+				{
+					(*itera)->set_nickname(names);
+					message_join((*itera), RPL_NAMREPLY((*itera)->get_nickname(), k[1], names));
+				}
+			}
+		}
+	}
+	else if(k[2] == "-o")
+	{
+		for (std::vector<Channel>::iterator itChannels = channels.begin(); itChannels != channels.end(); itChannels++)
+		{
+			if(itChannels->ChannelName == k[1])
+			{
+				sendmessage(ite, "Mode " + itChannels->ChannelName + " -o " + k[3]);
+				for(std::vector <Client *>::iterator itera = itChannels->users.begin() ; itera != itChannels->users.end(); itera++)
+				{
+					message_join((*itera), RPL_NAMREPLY((*itera)->get_nickname(), k[1], k[3]));
+				}
+			}
+		}
+	}
+}
